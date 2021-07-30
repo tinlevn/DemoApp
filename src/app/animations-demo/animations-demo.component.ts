291431ec85a3ff787bca69e01e7c9d2a57293921
Sunil patel <chemsp@outlook.com>
2021-07-30 16:51:54 -0400

    more animations added

--- src/app/animations-demo/animations-demo.component.ts
+++ src/app/animations-demo/animations-demo.component.ts
@@ -5,8 +5,10 @@ import {
   style,
   animate,
   transition,
+  keyframes,
   // ...
 } from '@angular/animations';
+import { User } from '../data/user';
 
 @Component({
   selector: 'app-animations-demo',
@@ -32,11 +34,88 @@ import {
         animate('0.5s')
       ]),
     ]),
+
+    trigger('list', [
+      state('starting', style({
+        opacity: 1, transform: 'translateX(0px)'
+      })),
+      transition('void=>*', [
+        style({
+          opacity: 0, transform: 'translateX(-100px)'
+        }),
+        animate(300)
+      ]),
+      transition('*=>void', [
+
+        animate(300, style({
+          opacity: 0, transform: 'translateX(100px)'
+        }))
+      ]),
+
+    ]),
+
+    trigger('list2', [
+      state('starting', style({
+        opacity: 1, transform: 'translateX(0px)'
+      })),
+
+      transition('void=>*', [
+        animate(1000, keyframes([
+          style({
+            opacity: 1, transform: 'translateX(600px)', offset: '0'
+          }),
+          style({
+            opacity: 0.6, transform: 'translateX(300px)', offset: '.3'
+          }),
+          style({
+            opacity: 0.3, transform: 'translateX(150px)', offset: '0.6'
+          }),
+          style({
+            opacity: 0, transform: 'translateX(0px)', offset: '1'
+          }),
+        ]))
+      ])
+      ,
+
+      transition('*=>void', [
+        animate(2000, keyframes([
+          style({
+            opacity: 1, transform: 'translateX(0px)', offset: '0'
+          }),
+          style({
+            opacity: 0.6, transform: 'translateX(150px)', offset: '.3'
+          }),
+          style({
+            opacity: 0.3, transform: 'translateX(300px)', offset: '0.6'
+          }),
+          style({
+            opacity: 0, transform: 'translateX(600px)', offset: '1'
+          }),
+        ]))
+      ]),
+
+    ])
   ]
 })
 export class AnimationsDemoComponent implements OnInit {
 
   isOpen = true;
+  users: User[] = [
+    { name: "Paul" },
+    { name: "Kelly" },
+    { name: "Raj" },
+    { name: "Sam" },
+    { name: "Roger" },
+    { name: "Lily" },
+  ];
+  users2: User[] = [
+    { name: "Paul" },
+    { name: "Kelly" },
+    { name: "Raj" },
+    { name: "Sam" },
+    { name: "Roger" },
+    { name: "Lily" },
+  ];
 
 
   constructor() { }
@@ -47,4 +126,36 @@ export class AnimationsDemoComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  removeUser(i: number) {
+    if (i > -1) {
+      this.users.splice(i, 1);
+    }
+
+  }
+
+  addUser(username: string) {
+    if (username) {
+      let user = { name: username };
+      this.users.splice(0, 0, user);
+    }
+
+  }
+
+
+  removeUser2(i: number) {
+    if (i > -1) {
+      this.users2.splice(i, 1);
+    }
+
+  }
+
+  addUser2(username: string) {
+    if (username) {
+      let user = { name: username };
+      this.users2.splice(0, 0, user);
+    }
+
+  }
+
 }
+
